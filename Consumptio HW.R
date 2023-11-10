eba3742008256cf9286e371ce186210fc0bbc470
aaryanm0705 <mallayanmath.aaryan@gmail.com>
2023-11-10 21:51:32 +0100

    First Commit

--- Consumptio HW.R
+++
@@ -1,8 +0,0 @@
-coalts= ts(coal$Value,start = c(1965,1),end = c(2010), frequency = 1)
-plot(coalts)
-model= HoltWinters(coalts,beta= FALSE,gamma = FALSE)
-model
-plot(model)
-forecast::holt(coalts,h=10)
-plot(forecast::holt(coalts,h=10))
- #how to calculate RMSE
